5f8a7d21e4192d23ffede460562d7cc47a4a9c22
Joy Phoebe <joyapisi@gmail.com>
2023-01-03 18:21:39 +0300

    add classes and functions to node.js to make a pop up menu on the HTML navigation bar

--- node.js
+++ node.js
@@ -1,6 +1,30 @@
-let navigataionBar = document.querySelector('ul-nav');
-let navigationToggle = document.querySelector('nav-button2');
-navigationToggle.addEventListener('click', function (event)
-{
-navigataionBar.classList.toggle('active')
-});
+let navigataionBar = document.querySelector('.ul-nav');
+let navigationMenuItems = document.querySelectorAll('navigationList');
+let hamburgerButton = document.querySelector('.nav-button2');
+let closeIcon= document.querySelector(".closeIcon");
+let menuIcon = document.querySelector(".menuIcon");
+
+// hamburgerButton.addEventListener('click', function (event) {
+// navigataionBar.classList.toggle('active')
+// });
+
+function hamburgerMenu() {
+    if (navigataionBar.classList.contains("showMenu")) {
+    navigataionBar.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+    } 
+    else {
+    navigataionBar.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+    }
+  }
+  
+  hamburgerButton.addEventListener("click", hamburgerMenu);
+
+  navigationMenuItems.forEach( 
+    function(navigationList) { 
+        navigationList.addEventListener("click", hamburgerMenu);
+    }
+  )
