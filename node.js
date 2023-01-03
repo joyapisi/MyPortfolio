3b830cf4949ce29278a8161a57ceb7c203856c50
Joy Phoebe <joyapisi@gmail.com>
2023-01-03 12:09:43 +0300

    add js linters and node.js file

---
+++ node.js
@@ -0,0 +1,6 @@
+let navigataionBar = document.querySelector('ul-nav');
+let navigationToggle = document.querySelector('nav-button2');
+navigationToggle.addEventListener('click', function (event)
+{
+navigataionBar.classList.toggle('active')
+});
