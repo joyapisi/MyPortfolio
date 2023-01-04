4e191fde79464a2f337a5dc76e1e67cb2a20f7ba
Joy Phoebe <joyapisi@gmail.com>
2023-01-04 12:38:27 +0300

    add styles to style.css

--- node.js
+++ node.js
@@ -1,29 +1,25 @@
 let navigataionBar = document.querySelector('.ul-nav');
-let navigationMenuItems = document.querySelectorAll('navigationList');
+let navigationList = document.querySelectorAll('navigationList');
 let hamburgerButton = document.querySelector('.nav-button2');
 let closeIcon= document.querySelector(".closeIcon");
-let menuIcon = document.querySelector(".menuIcon");
-
-// hamburgerButton.addEventListener('click', function (event) {
-// navigataionBar.classList.toggle('active')
-// });
+// let menuIcon = document.querySelector(".menuIcon");
 
 function hamburgerMenu() {
-    if (navigataionBar.classList.contains("showMenu")) {
+    if 
+    (navigataionBar.classList.contains("showMenu")) {
     navigataionBar.classList.remove("showMenu");
     closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-    } 
-    else {
+    hamburgerButton.style.display = "block";
+  } else {
     navigataionBar.classList.add("showMenu");
     closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-    }
-  }
+    hamburgerButton.style.display = "none";
+ }
+}
   
   hamburgerButton.addEventListener("click", hamburgerMenu);
 
-  navigationMenuItems.forEach( 
+  navigationList.forEach( 
     function(navigationList) { 
         navigationList.addEventListener("click", hamburgerMenu);
     }
