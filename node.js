ca0fd72a55207911a062ca73364c364e357234c7
Kwaku28 <rekenburg777@gmail.com>
2023-01-04 13:08:06 +0000

    manipulate to toggle menu

--- node.js
+++ node.js
@@ -1,26 +1,14 @@
-let navigataionBar = document.querySelector('.ul-nav');
-let navigationList = document.querySelectorAll('navigationList');
-let hamburgerButton = document.querySelector('.nav-button2');
-let closeIcon= document.querySelector(".closeIcon");
-// let menuIcon = document.querySelector(".menuIcon");
+const navButton2 = document.querySelector(".nav-button2");
+const navMenu = document.querySelector(".ul-nav");
 
-function hamburgerMenu() {
-    if 
-    (navigataionBar.classList.contains("showMenu")) {
-    navigataionBar.classList.remove("showMenu");
-    closeIcon.style.display = "none";
-    hamburgerButton.style.display = "block";
-  } else {
-    navigataionBar.classList.add("showMenu");
-    closeIcon.style.display = "block";
-    hamburgerButton.style.display = "none";
- }
-}
-  
-  hamburgerButton.addEventListener("click", hamburgerMenu);
+navButton2.addEventListener("click", () => {
+  navButton2.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-  navigationList.forEach( 
-    function(navigationList) { 
-        navigationList.addEventListener("click", hamburgerMenu);
-    }
-  )
+document.querySelectorAll("ul.ul-nav > li").forEach((n) =>
+  n.addEventListener("click", () => {
+    navButton2.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
