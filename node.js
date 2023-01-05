92d7d25dc3bb5301b0bfecbdc7883b40f09a85d3
Joy Phoebe <joyapisi@gmail.com>
2023-01-05 08:10:19 +0300

    fix linter errors

--- node.js
+++ node.js
@@ -1,14 +1,12 @@
-const navButton2 = document.querySelector(".nav-button2");
-const navMenu = document.querySelector(".ul-nav");
+const navButton2 = document.querySelector('.nav-button2');
+const navMenu = document.querySelector('.ul-nav');
 
-navButton2.addEventListener("click", () => {
-  navButton2.classList.toggle("active");
-  navMenu.classList.toggle("active");
+navButton2.addEventListener('click', () => {
+  navButton2.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll("ul.ul-nav > li").forEach((n) =>
-  n.addEventListener("click", () => {
-    navButton2.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
+document.querySelectorAll('ul.ul-nav > li').forEach((n) => n.addEventListener('click' () => {
+    navButton2.classList.remove('active');
+    navMenu.classList.remove('active');
+  }));
