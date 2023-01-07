0e791f3bbe23300f1518e7dc88ba75756d7a4839
Kwaku28 <rekenburg777@gmail.com>
2023-01-07 21:32:40 +0000

    Javascript code added to execute the popup window

--- node.js
+++ node.js
@@ -50,3 +50,40 @@ if (informationStored) {
     element.value = informationStored[element.name];
   });
 }
+
+const openModelButtons = document.querySelectorAll('[data-model-target]');
+const closeModelButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModelButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const model = document.querySelector(button.dataset.modelTarget);
+    openModel(model);
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const models = document.querySelectorAll('.model.active')
+  models.forEach(model => {
+    closeModel(model);
+  })
+})
+
+closeModelButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const model = button.closest('.model');
+    closeModel(model);
+  })
+})
+
+function openModel(model) {
+  if (model == null) return;
+  model.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModel(model) {
+  if (model == null) return;
+  model.classList.remove('active');
+  overlay.classList.remove('active');
+}
